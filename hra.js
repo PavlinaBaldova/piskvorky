3a43a4458216d327c48c27873e888f411a3eecd2
PavlinaBaldova <pavlina.bal@seznam.cz>
2020-10-24 09:26:59 +0200

    Animation in progress

--- hra.js
+++ hra.js
@@ -31,25 +31,26 @@ const game = () => {
 let player = "circle";
 
 const play = (event) => {
-   if (player === "circle") {
+  if (player === "circle") {
     event.target.classList.add('board__field--circle');
     event.target.disabled = true;
+    const tabsPlay = document.querySelector('.tabs');
+    tabsPlay.innerHTML = `<p class="tabs tabs__play">Hraje:<img
+    class= "icon" src="images/circle.svg"
+    alt="Ikona kolečka"/></p>`;
     player = "cross";
   } else if (player === "cross") {
-     event.target.classList.add('board__field--cross');
-     event.target.disabled = true;
-     player = "circle";
+    event.target.classList.add('board__field--cross');
+    event.target.disabled = true;
+    const tabsPlay = document.querySelector('.tabs');
+    tabsPlay.innerHTML = `<p class="tabs tabs__play">Hraje:<img
+      class= "icon" src="images/cross.svg"
+      alt="Ikona křížku"/></p>`;
+    player = "circle";
   }
 }
 
-  const buttons = document.querySelectorAll('button');
-  for (let i = 0; i < buttons.length; i += 1) {
-    buttons[i].addEventListener('click', play);
-  }
-
-
-
-
-
-
-  
+const buttons = document.querySelectorAll('button');
+for (let i = 0; i < buttons.length; i += 1) {
+  buttons[i].addEventListener('click', play);
+}
